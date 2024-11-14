801572d4f324e06816d36a6d18186d3cfae31316
AndrewDRichter <andrewrichter1@hotmail.com>
2024-11-14 17:04:24 -0300

    starting using drf with serializers

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_create_list_view, genre_detail_update_view
+# from genres.views import genre_create_list_view, genre_detail_update_view
+from genres.views import GenreCreateListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_create_list_view, name="genre-create-list"),
-    path('genres/<int:id>', genre_detail_update_view, name='genre-detail-update')
+    path('genres/', GenreCreateListView.as_view(), name="genre-create-list"),
+    # path('genres/<int:id>', genre_detail_update_view, name='genre-detail-update')
 ]

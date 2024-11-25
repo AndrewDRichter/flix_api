c5b9409f6293db3775bf132b47f335be7d5c9bc1
AndrewDRichter <andrewrichter1@hotmail.com>
2024-11-25 14:51:46 -0300

    updated movie serializer validation

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,7 +2,20 @@ from rest_framework import serializers
 from .models import Movie
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def get_rate(self, obj):
+        return 5
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1900.')
+        return value
+    
+    def validate_summary(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('Summary não deve conter mais de 500 caracteres.')
